2057e477c2a54b4024594d3446e9d30b51c9f0c9
Jonathan  Zylberberg <zylberberg.jonathan@gmail.com>
2016-11-15 07:22:22 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+    i = 0  # put argument(s) here
+    while i < collection.length
+      yield(collection[i])
+      i +=1
+    end
+    collection
+end
+
+my_each(collection) do |i|
+  puts i
 end
